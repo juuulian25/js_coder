f8d93a5ff1fc8b4ef82b5b8d71dab329aa7e57b5
Julian Novizky <julinovizky@gmail.com>
2022-07-27 18:51:17 -0300

    trabajando con dom

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -139,22 +139,22 @@ function CrearZapatilla (marca, talle, precio) {
 }
 
 //zapatillas que contengan letra A
-
+/*
 for (const productos of zapas){
     console.log(productos.marca, productos.talle, productos.precio);
 }
-
+*/
 const resultadoA = zapas.filter((el) => el.marca.includes("a"))
 
 console.log(resultadoA)
 
 
 //zapatillas que contengan letra E
-
+/*
 for (const productos of zapas){
     console.log(productos.marca, productos.talle, productos.precio);
 }
-
+*/
 const resultadoE = zapas.filter((el) => el.marca.includes("e"))
 
 console.log(resultadoE)
@@ -163,12 +163,12 @@ console.log(resultadoE)
 
 //precio mayor a 25
 
-console.log("Todos estos productos cuestan mas de $26")
-
+console.log("Todos estos productos cuestan mas de $25")
+/*
 for (const productos of zapas){
     console.log(productos.marca, productos.talle, productos.precio);
 }
-
+*/
 const resultadoPrecio = zapas.filter((el) => el.precio>25)
 
 console.log(resultadoPrecio)
@@ -194,3 +194,201 @@ function numero_x (j){
 let esIgual = numero_x(8)
 console.log(esIgual(8))
 console.log(esIgual(9))
+
+
+/////////////////////////////////
+
+//arrancamos con dom
+/*
+let domElemento
+domElemento=document.all
+domElemento=document.head
+domElemento=document.body
+domElemento=document.scripts
+domElemento=document.images
+
+
+console.log(domElemento)
+*/
+document.addEventListener("DOMContentLoaded", function(){
+    //Tus funciones
+
+
+
+let cardtitle = document.querySelector(".card-title");
+console.log(cardtitle)
+
+let cardbody=document.querySelector(".card-body")
+console.log(cardbody)
+
+let h5 = document.querySelector("h5")
+console.log(h5)
+
+let allcardtitle = document.querySelectorAll(".card-title")
+console.log(allcardtitle)
+
+let probandollamardom = document.querySelector("#probando-llamar-dom")
+console.log(probandollamardom)
+
+// reemplazando titulo a la pagina
+
+const encabezado = document.querySelector("h1").textContent="Nuevo heading"
+console.log(encabezado)
+
+
+//cambiando nombre a zapatillas puma
+const etiquetaPuma = document.querySelector("#probando-llamar-dom").textContent="cambiando nombre a Puma desde JS"
+console.log(etiquetaPuma)
+
+//cambiando text a consultar stock zapa adidas
+
+const probandoSiHayStock = document.querySelector("#tieneStock").textContent="Hay Stock. No hace falta consultar."
+console.log(probandoSiHayStock)
+
+
+
+
+const subtituloOff = document.querySelector("h2")
+subtituloOff.remove()
+
+let agregandoSubtitulo = document.createElement("h2")
+agregandoSubtitulo.innerHTML="<p>Agregando Subtitulo via JS</p>"
+
+const encabeZado = document.querySelector(".encabeZado")
+console.log(encabeZado.children)
+encabeZado.insertBefore(agregandoSubtitulo, encabeZado.children[0])
+
+
+//agregando array en pantalla
+
+let listaVacia = document.querySelector("#listaVacia")
+
+let otrosCursos = [
+    "Nike",
+    "Adidas",
+    "Fila",
+    "Puma"
+]
+
+for (let curso of otrosCursos){
+    let listado=document.createElement("div")
+    listado.innerHTML=curso
+    listaVacia.appendChild(listado)
+}
+
+
+//agregando array
+
+
+const zapaTillas = [
+    {id: 1, titulo:"Nike", precio: 43},
+    {id: 2, titulo:"Adidas", precio: 55},
+    {id: 3, titulo:"Puma", precio: 44},
+    {id: 4, titulo:"Fila", precio: 23},
+];
+
+
+
+
+for ( const probandoArray of zapaTillas){
+    let li = document.createElement("div");
+    
+    li.innerHTML = `<h3>ID: ${probandoArray.id}</h3>
+                    <p>Producto: ${probandoArray.titulo}</p>
+                    <b>$ ${probandoArray.precio}</b>`;
+    
+    document.body.appendChild(li);
+    
+    }
+    
+
+    //interactuando con html
+
+
+
+    console.log("Que zapatilla quiere comprar? Nike, Adidas, Puma o Fila?")
+
+   let mensajePantalla = parseInt(prompt("Ingrese 1 = Nike, 2 = Adidas, 3 = Puma, 4 = Fila"))
+
+    if (mensajePantalla==1){
+        
+        let li = document.createElement("div");
+    
+        li.innerHTML = `<h3>ID: ${zapaTillas[0].id}</h3>
+                        <p>Producto: ${zapaTillas[0].titulo}</p>
+                        <b>$ ${zapaTillas[0].precio}</b>`;
+                        document.body.appendChild(li);
+
+    } else if (mensajePantalla==2){
+        let li = document.createElement("div");
+    
+        li.innerHTML = `<h3>ID: ${zapaTillas[1].id}</h3>
+                        <p>Producto: ${zapaTillas[1].titulo}</p>
+                        <b>$ ${zapaTillas[1].precio}</b>`;
+                        document.body.appendChild(li);
+    }
+    else if (mensajePantalla==3){
+        let li = document.createElement("div");
+    
+        li.innerHTML = `<h3>ID: ${zapaTillas[2].id}</h3>
+                        <p>Producto: ${zapaTillas[2].titulo}</p>
+                        <b>$ ${zapaTillas[2].precio}</b>`;
+                        document.body.appendChild(li);
+    }
+  else if (mensajePantalla==4){
+        let li = document.createElement("div");
+    
+        li.innerHTML = `<h3>ID: ${zapaTillas[3].id}</h3>
+                        <p>Producto: ${zapaTillas[3].titulo}</p>
+                        <b>$ ${zapaTillas[3].precio}</b>`;
+                        document.body.appendChild(li);
+    }
+
+    else {
+        alert("El numero ingresado no es valido.")
+    }
+
+
+
+
+
+
+
+
+/*
+function cargarHTML(){
+
+    zapaTillas.forEach( id => { 
+        let lista = document.getElementById("s1")
+        lista.insertAdjacentHTML
+    })
+}
+*/
+
+
+
+
+//plantillas literales
+/*
+let zapaTillas = 
+    {id: 1, titulo:"Nike", precio: 55};
+  /*  {id: 2, titulo:"Adidas", precio: 55}, /funciona solo con un array
+    {id: 3, titulo:"Puma", precio: 55},
+
+
+let concatenado = "id:" + zapaTillas.id + "- producto:" + zapaTillas.titulo + "$"+zapaTillas.precio;
+
+let plantilla =`id: ${zapaTillas.id} - Nombre ${zapaTillas.titulo} $${zapaTillas.precio}`;
+
+console.log(concatenado)
+console.log(plantilla)
+*/
+
+
+}
+
+)
+
+;
+
+
